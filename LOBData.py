42d60a1fbdb5da39aa1816b3791b014b3d32939f
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2020-09-11 17:37:08 +0100

    Use static cache file and return (all) data from cache

--- LOBData.py
+++ LOBData.py
@@ -18,15 +18,23 @@ class LOBData:
         self.security = security
         self.frequency = frequency
         self.caching_folder = f'{root_caching_folder}/{security}'
+        self.cache_file = f'{self.caching_folder}/data-cache.csv'
         self.levels = levels
 
         os.makedirs(self.caching_folder, exist_ok=True)
-        self.save_top_n_levels()
 
-    def get_df(self, start, end):
-        self.save_top_n_levels()
+    def get_LOB_data(self, start, end):
 
-    def save_top_n_levels(self):
+        if not os.path.isfile(self.cache_file):
+            self.generate_cache_file()
+
+        df = pd.read_csv(self.cache_file)
+        # TODO - check if within available dates and return from start till end
+
+        return df
+
+    def generate_cache_file(self):
+
         all_files = []
         for root, subdirs, files in os.walk(f'{self.raw_data_path}/{self.security}'):
             for filename in files:
@@ -36,61 +44,47 @@ class LOBData:
         first = all_files[0].split('.')[0] # get 20200403_13 from 20200711_14.json.gz
         last = all_files[-1].split('.')[0]
 
+        # deliberately calculate all time steps instead of using 'first' and 'last' to check for missing files
+        start_date = datetime.strptime(first, '%Y%m%d_%H')
+        end_date = datetime.strptime(last, '%Y%m%d_%H')
 
-        cache_file = f'{self.caching_folder}/{self.levels}_levels-{first}-{last}.csv'
+        # iterate through all time steps between start and end date
+        print('Processing raw hourly snapshot files:')
+        processed_data = []
 
-        if not os.path.isfile(cache_file):
+        while start_date <= end_date:
+            subdir = datetime.strftime(start_date, '%Y/%m/%d')
+            filename = datetime.strftime(start_date, '%Y%m%d_%H.json.gz')
+            path_string = f'{self.raw_data_path}/{self.security}/{subdir}/{filename}'
+            print(path_string)
 
-            # deliberately calculate all time steps instead of using 'first' and 'last' to check for missing files
-            start_date = datetime.strptime(first, '%Y%m%d_%H')
-            end_date = datetime.strptime(last, '%Y%m%d_%H')
+            try:
+                # load raw data
+                with gzip.open(path_string, 'r') as f:  # gzip
+                    json_bytes = f.read()               # bytes
+                raw_one_hour_data = json.loads(json_bytes.decode('utf-8'))
 
-            # iterate through all time steps between start and end date
-            print('Processing raw hourly snapshot files:')
-            processed_data = []
+                for key in raw_one_hour_data.keys():
+                    # unravel the nested json structure into a more manageable list of lists
+                    processed_data.append(list(zip(
+                        [i[0] for i in raw_one_hour_data.get(key)['asks'][0:self.levels]], # ask px
+                        [i[1] for i in raw_one_hour_data.get(key)['asks'][0:self.levels]], # ask size
+                        [i[0] for i in raw_one_hour_data.get(key)['bids'][0:self.levels]], # bid px
+                        [i[1] for i in raw_one_hour_data.get(key)['bids'][0:self.levels]], # bid size
+                        list(range(self.levels)), # ob level - assuming same for both
+                        [key[-15:]] * self.levels  # datetime part of the key
+                    )))
 
-            while start_date <= end_date:
-                subdir = datetime.strftime(start_date, '%Y/%m/%d')
-                filename = datetime.strftime(start_date, '%Y%m%d_%H.json.gz')
-                path_string = f'{self.raw_data_path}/{self.security}/{subdir}/{filename}'
-                print(path_string)
+            except IOError as e:
+                print(e.errno)
+                print(e)
 
-                try:
-                    # load raw data
-                    with gzip.open(path_string, 'r') as f:  # gzip
-                        json_bytes = f.read()               # bytes
-                    raw_one_hour_data = json.loads(json_bytes.decode('utf-8'))
+            start_date += timedelta(hours=1)
 
-                    for key in raw_one_hour_data.keys():
-                        # unravel the nested json structure into a more manageable list of lists
-                        processed_data.append(list(zip(
-                            [i[0] for i in raw_one_hour_data.get(key)['asks'][0:self.levels]], # ask px
-                            [i[1] for i in raw_one_hour_data.get(key)['asks'][0:self.levels]], # ask size
-                            [i[0] for i in raw_one_hour_data.get(key)['bids'][0:self.levels]], # bid px
-                            [i[1] for i in raw_one_hour_data.get(key)['bids'][0:self.levels]], # bid size
-                            list(range(self.levels)), # ob level - assuming same for both
-                            [key[-15:]] * self.levels  # datetime part of the key                                    
-                        ))) 
+        df = pd.DataFrame(processed_data)
+        df.to_csv(self.cache_file)
 
-                except IOError as e:
-                    print(e.errno)
-                    print(e)
-
-                start_date += timedelta(hours=1)
-
-            df = pd.DataFrame(processed_data)
-            df.to_csv(cache_file)
-
-    # method that unzips a single file and returns the dictionary like content
-    def load_json(self, date, time):
-
-        path_string = f'{self.raw_data_path}/{self.security}/{date}/{date.replace("/", "")}_{time}.json.gz'
-
-
-
-    def resample_raw_files(self, start_date, end_date):
-        numdays = (end_date - start_date).days
-
+#    def resample(self, start_date, end_date):
 
 
 root_path = '/home/pawel/Documents/LOB-data/new' # path where zipped files are stored
@@ -102,4 +96,4 @@ end = datetime(2020, 8, 20) # first day we captured data
 
 # instantiate class
 data = LOBData(root_path, security, root_caching_folder, frequency)
-data.get_df(start, end)
+data.get_LOB_data(start, end)
