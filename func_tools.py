fixed z score bug and improved funtionalities for data normalisation

func_tools.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 
-def normalize(ts, norm_type='z', roll=0):
+def normalize(ts, norm_type='z_score', roll=0):
     '''
     Function to normalize timeseries
 
@@ -13,10 +13,17 @@ def normalize(ts, norm_type='z', roll=0):
 
     Returns: pandas series
     '''
-
+    
     if norm_type=='z_score':
-        return (ts-ts.mean()) / ts.std()
-
+        
+        if ts.shape[1] > 1:
+            ts_stacked = ts.stack()
+        else:
+            ts_stacked = ts
+        
+        return (ts-ts_stacked.mean()) / ts_stacked.std()
+    
+    # dynamic can't accomodate multi columns normalization yet
     elif norm_type=='dyn_z_score' and type(roll) is int and roll>0:
         return (ts - ts.rolling(roll).mean().shift(1) 
               ) / ts.rolling(roll).std(ddof=0).shift(1)
@@ -37,7 +44,7 @@ def get_labels(ts, k, alpha):
     '''
 
     m_minus = ts.rolling(k).mean() # mean prev k prices
-    m_plus = ts.shift(-k*2).rolling(k).mean() # # mean next k prices
+    m_plus = ts.shift(-k).rolling(k).mean() # # mean next k prices
 
     # direction of price movements at time t
     direction = (m_plus - m_minus) / m_minus
@@ -76,6 +83,37 @@ def get_pnl(px_ts, labels, long_only=True):
                                    else (x['return']*-1 if x['labels']==-1 else 0), axis=1) + 1
         
         return df[(df['labels']==1) | (df['labels']==-1)]['ls_return'].cumprod() - 1
+
+
+
+def cnn_data_reshaping(X, Y, T):
+    '''
+    Reshape/augment data for 1D convolutions
+    Inputs: X -> np.array with shape (lentgh_timeseries, # entries * order book depth for each timestamp)
+            Y -> np.array with shape (length timeseries, 1)
+            T -> int: # past timesteps to augment each timestamp
+
+    Output: reshaped X and Y
+
+    To do: accomodate for 2D convs
+    '''
+    [N, D] = X.shape
+    df = np.array(X)
+
+    dY = np.array(Y)
+
+    dataY = dY[T - 1:N]
+
+    dataX = np.zeros((N - T + 1, T, D))
+
+    for i in range(T, N + 1):
+        dataX[i - T] = df[i - T:i, :]
+
+    dataX = dataX.reshape(dataX.shape)# + (1,)) # no need to add the extra dimension for 1d conv
+
+    print(f'shape X:{dataX.shape}, shape Y:{dataY.shape}')
+
+    return dataX, dataY
 
 
 def plot_labels(labels):
@@ -143,4 +181,8 @@ def plot_labels(labels):
                             for i in range(len(label_change)-1)
     ]
     return background_color
+
+
+
+
 