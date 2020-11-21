69602013d543dae3d03ef6d2592cdac864ae8429
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2020-11-21 23:08:37 +0000

    Processing new file format (WIP)

--- LOBData.py
+++ LOBData.py
@@ -1,11 +1,10 @@
 import gzip
 import json
-import glob, os
+import os
 
-from datetime import datetime, timedelta, time
+from datetime import datetime, timedelta
 
 import pandas as pd
-import numpy as np
 
 class LOBData:
 
@@ -35,36 +34,40 @@ class LOBData:
                     all_files.append(filename) if filename.endswith('.json.gz') else None
             all_files.sort()
 
-            first = all_files[0].split('.')[0] # get 20200403_13 from 20200403_13.json.gz
-            last = all_files[-1].split('.')[0]
+            first = all_files[0].split('.')[0].split('-')[0] # get 20200403_13 from 20200403_13.json.gz or 20200403_13-1-2.json.gz
+            last = all_files[-1].split('.')[0].split('-')[0]
             start_date = datetime.strptime(first, '%Y%m%d_%H')
             end_date = datetime.strptime(last, '%Y%m%d_%H')
 
-            print('Processing raw hourly snapshot files:')
+            print(f'Processing data from {start_date} to {end_date}')
             processed_data = []
 
-            # Loop through all time steps between start and end date instead
-            # of looping through all_files list to check for missing files
-            while start_date <= end_date:
-                subdir = datetime.strftime(start_date, '%Y/%m/%d')
-                filename = datetime.strftime(start_date, '%Y%m%d_%H.json.gz')
-                path = f'{self.raw_data_path}/{self.security}/{subdir}/{filename}'
-                print(path)
-                raw_one_hour_data = self.load_data_file(path)
+            # Loop through day directories
+            date_to_process = start_date
+            while date_to_process <= end_date:
+                day_folder = datetime.strftime(date_to_process, '%Y/%m/%d')
+                # TODO - cache one day with per levels depth and read from cache if already processed
 
-                for key in raw_one_hour_data.keys():
-                    # unravel the nested json structure into a more manageable list of lists
-                    processed_data.append(list(zip(
-                        [i[0] for i in raw_one_hour_data.get(key)['asks'][0:self.levels]], # ask px
-                        [i[1] for i in raw_one_hour_data.get(key)['asks'][0:self.levels]], # ask size
-                        [i[0] for i in raw_one_hour_data.get(key)['bids'][0:self.levels]], # bid px
-                        [i[1] for i in raw_one_hour_data.get(key)['bids'][0:self.levels]], # bid size
-                        list(range(self.levels)), # ob level - assuming same for both
-                        [key[-15:]] * self.levels  # datetime part of the key
-                    )))
+                for filename in os.listdir(f'{self.raw_data_path}/{self.security}/{day_folder}'):
+                    print(f'Reading {self.security}/{filename}')
+                    raw_data = self.load_data_file(f'{self.raw_data_path}/{self.security}/{day_folder}/{filename}')
 
-                start_date += timedelta(hours=1)
+                    # TODO - datetime as keys to sort later
+                    for key in raw_data.keys():
+                        # unravel the nested json structure into a more manageable list of lists
+                        processed_data.append(list(zip(
+                            [i[0] for i in raw_data.get(key)['asks'][0:self.levels]], # ask px
+                            [i[1] for i in raw_data.get(key)['asks'][0:self.levels]], # ask size
+                            [i[0] for i in raw_data.get(key)['bids'][0:self.levels]], # bid px
+                            [i[1] for i in raw_data.get(key)['bids'][0:self.levels]], # bid size
+                            list(range(self.levels)), # ob level - assuming same for both
+                            [key[-15:]] * self.levels  # datetime part of the key
+                        )))
+                # TODO sort datetime keys and cache one day as csv?
+
+                date_to_process += timedelta(days=1) # the most nested folder is a day of the month
             
+
             # unravel nested structure and force data types
             df = pd.DataFrame([y for x in processed_data for y in x], #flatten the list of lists structure
                             columns = ['Ask_Price', 'Ask_Size', 'Bid_Price', 'Bid_Size','Level', 'Datetime'])
@@ -83,29 +86,18 @@ class LOBData:
             resampled_data.to_csv(self.cache_file)
             return df
 
-    def load_data_file(self, path_string):
-
+    def load_data_file(self, path):
         try:
-            # load raw data
-            with gzip.open(path_string, 'r') as f:  # gzip
-                json_bytes = f.read()               # bytes
-                json_string = json_bytes.decode('utf-8')
-
+            with gzip.open(path, 'r') as f:
+                json_string = f.read().decode('utf-8')
                 frozen = json_string.count('"isFrozen": "1"')
                 if frozen > 0:
                     print(f'Frozen {frozen} snapshots')
-            one_hour_data = self.load_json(json_string)
-
-        except IOError as e:
-            print(e.errno)
-            print(e)
-            # TODO Handle missing files
+            return self.load_json(json_string)
 
         except Exception as e:
             print(e.errno)
             print(e)
-
-        return one_hour_data
 
     def load_json(self, json_string):
         try:
@@ -157,9 +149,9 @@ class LOBData:
 
 # TODO add method which returns data with different frequency
 
-root_path = '/home/pawel/Documents/LOB-data/new' # path where zipped files are stored
+root_path = '/home/pawel/Documents/LOB-data/new-format-test' # path where zipped files are stored
 root_caching_folder = '/home/pawel/Documents/LOB-data/cache' # processed cached data folder
-security = 'BTC_ETH3'
+security = 'BNB_BTC'
 
 # instantiate class
 data = LOBData(root_path, security, root_caching_folder)
